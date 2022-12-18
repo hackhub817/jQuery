0854a1677a7d819adc92e422efee9a50198a47ed
hackhub817 <piyushguptaji123@gmail.com>
2022-12-18 18:48:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+$("h1").text("BYe");
+
+//document.querySelectorAll("button");
+//similar in jquery
+$("button");
+
+
+//document.querySelectorAll("button").innerHTML="BYR";
+//$("button").text("fuck off")
+
+
+$("button").html("<em>HEY<em>")
